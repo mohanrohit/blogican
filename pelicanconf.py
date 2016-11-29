4f3dfd123a28143f85f54c5067252af11873ab5d
Rohit Mohan <rohit.mohan@gmail.com>
2016-11-29 14:12:57 -0500

    initial set of files for blog

---
+++ pelicanconf.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+import os
+current_directory = os.getcwd()
+
+SITENAME = u"Pelican"
+SITEURL = ''
+RELATIVE_URLS = True
+
+AUTHOR = u'Rohit Mohan'
+
+IGNORE_FILES = ["web-development-python-and-flask/*"]
+
+SUMMARY_MAX_LENGTH = 50
+
+# theming
+
+THEME = current_directory + "/theme/pelican-bootstrap3-custom"
+
+BOOTSTRAP_THEME = "journal"
+PYGMENTS_STYLE = "vs"
+DISPLAY_BREADCRUMBS = True
+DISPLAY_CATEGORY_IN_BREADCRUMBS = True
+
+DISPLAY_TAGS_ON_SIDEBAR = True
+DISPLAY_TAGS_INLINE = False
+DISPLAY_RECENT_POSTS_ON_SIDEBAR = True
+
+DISPLAY_CATEGORIES_ON_SIDEBAR = True
+
+PLUGIN_PATHS = [current_directory + "/plugins"]
+#PLUGINS = ["series", "tag_cloud"]
+PLUGINS = ["series"]
+
+DISPLAY_SERIES_ON_SIDEBAR = False
+SHOW_SERIES = False
+SERIES_TEXT = "" #"Part <strong>%(index)s</strong> of the <strong>%(name)s</strong> series"
+
+SHOW_ARTICLE_AUTHOR = True
+
+# end theming
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+ARTICLE_URL = '{category}/{date:%Y}/{date:%m}/{date:%d}/{slug}'
+ARTICLE_SAVE_AS = '{category}/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+
+DRAFT_URL = '{category}/{date:%Y}/{date:%m}/{date:%d}/{slug}'
+DRAFT_SAVE_AS = '{category}/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+
+PAGE_URL = 'pages/{slug}'
+PAGE_SAVE_AS = 'pages/{slug}.html'
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+
+LOAD_CONTENT_CACHE = True
