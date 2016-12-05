7f1af1b8c45f8bef16d455d68ceffdf46a1a29d0
Rohit Mohan <rohit.mohan@gmail.com>
2016-12-05 14:23:55 -0500

    added env variable PELICAN_SITEURL for dev on multiple machines; added background for sidebar; added list of articles for series on sidebar

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 
 from pelicanconf import *
 
-SITEURL = 'http://localhost:8000'
+SITEURL = os.environ.get("PELICAN_SITEURL") or "" # 'http://localhost:8000' <-- "ip-address:port"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
